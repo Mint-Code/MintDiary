d8e4409e053124ad1775d09dae923b0832b3fb48
Mint-Code <105038766+Mint-Code@users.noreply.github.com>
2023-01-18 21:44:05 +0800

    1.1.1 - New Features Version
    1. 新增一个日记模板。
    2. 增加了标题卡片。

--- MintDiary/Style/ButtonStyle.swift
+++ MintDiary/Style/ButtonStyle.swift
@@ -3,34 +3,37 @@ import SwiftUI
 // MARK: - ToolbarButtonStyle
 struct ToolbarButtonStyle: ButtonStyle {
     var level: Int
+    var isSelected: Bool
     
     @State private var isHover = false
 
-    init(level: Int) {
+    init(level: Int, isSelected: Bool = false) {
         self.level = level
+        self.isSelected = isSelected
     }
     
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .font(.headline)
+            .padding(.vertical, .cardToolbarButtonPaddingVertical)
+            .padding(.horizontal, .cardToolbarButtonPaddingHorizontal)
+            .foregroundColor(configuration.isPressed ? Color.textColor(level) : Color.secondaryColor(level))
+            .background(isSelected ? Color.opacityColor(level) : .clear)
+            .background(isHover ? Color.opacityColor(level) : .clear)
+            .background(configuration.isPressed ? Color.opacityColor(level) : .clear)
+            .cornerRadius(.cardToolbarButtonCornerRadius)
             .onHover { hover in
                 withAnimation(.easeInOut(duration: .animationTime)) {
                     self.isHover = hover
                 }
             }
-            .padding(.vertical, .cardToolbarButtonPaddingVertical)
-            .padding(.horizontal, .cardToolbarButtonPaddingHorizontal)
-            .foregroundColor(configuration.isPressed ? Color.textColor(level) : Color.secondaryColor(level))
-            .background(isHover ? Color.opacityColor(level) : .clear)
-            .background(configuration.isPressed ? Color.opacityColor(level) : .clear)
-            .cornerRadius(.cardToolbarButtonCornerRadius)
     }
 }
 
 // MARK: - View扩展
 extension View {
-    func toolbarButtonStyle(level: Int) -> some View {
+    func toolbarButtonStyle(level: Int, isSelected: Bool = false) -> some View {
         self
-            .buttonStyle(ToolbarButtonStyle(level: level))
+            .buttonStyle(ToolbarButtonStyle(level: level, isSelected: isSelected))
     }
 }
